47769928bc80a5a51ce4eae0022c42826d5b2d09
Thomas Zilz <t.zilz@mofux.org>
2017-09-11 12:13:28 +0200

    remove debug log

--- plugins/data/core.js
+++ plugins/data/core.js
@@ -34,8 +34,6 @@ module.exports = {
 			'by ' + req.session.config.hostname + ' (' + req.session.connection.server.address().address + ') with ' +
 			(req.session.accepted.ehlo ? 'ESMTP' : 'SMTP') + (req.session.secure ? 'S' : '') + (req.session.accepted.auth ? 'A' : '') + '; ' +
 			moment().locale('en').format('ddd, DD MMM YYYY HH:mm:ss ZZ') + '\r\n';
-			
-		console.log('received', received);
 
 		// write the received header to the top of the file
 		fileStream.write(received);
